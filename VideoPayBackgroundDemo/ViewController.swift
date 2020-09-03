ea58e44c2c0f94c27b562bd6bed68bbb631bd088
Rajesh Kumar <rajeshpremani68@gmail.com>
2020-09-03 20:14:21 +0500

    Initial Commit

---
+++ VideoPayBackgroundDemo/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  VideoPayBackgroundDemo
+//
+//  Created by Rajesh Kumar Sahil on 06/07/2020.
+//  Copyright © 2020 Rajesh Kumar Sahil. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var videoLayer: UIView!
+    @IBOutlet weak var signupBtn: UIButton!
+    
+    var player = AVPlayer()
+    var playerLayer = AVPlayerLayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        NotificationCenter.default.addObserver(self, selector: #selector(replayVideo(notification:)), name: .AVPlayerItemDidPlayToEndTime, object: player.currentItem)
+//        playVideo()
+    }
+
+    
+    func playVideo(){
+        guard let path = Bundle.main.path(forResource: "intro", ofType: "mp4") else {
+            return
+        }
+        
+        player = AVPlayer(url: URL(fileURLWithPath: path))
+        playerLayer = AVPlayerLayer(player: player)
+//        let player = AVPlayer(url: URL(fileURLWithPath: path))
+//        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        playerLayer.videoGravity = .resizeAspectFill
+        self.videoLayer.layer.addSublayer(playerLayer)
+        player.volume = 0
+        player.play()
+        
+        
+        videoLayer.bringSubviewToFront(image)
+        videoLayer.bringSubviewToFront(label)
+        videoLayer.bringSubviewToFront(loginBtn)
+        videoLayer.bringSubviewToFront(signupBtn)
+    }
+
+    @objc func replayVideo(notification: NSNotification){
+        self.player.seek(to: .zero)
+        self.player.play()
+    }
+}
+
---
+++ VideoPayBackgroundDemo/ViewControllerTwo.swift
@@ -0,0 +1,57 @@
+//
+//  ViewControllerTwo.swift
+//  VideoPayBackgroundDemo
+//
+//  Created by Rajesh Kumar Sahil on 07/07/2020.
+//  Copyright © 2020 Rajesh Kumar Sahil. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+
+    @IBOutlet weak var showBtn: UIButton!
+    let tableView = UITableView()
+    
+    let height:CGFloat = 250
+    var screenSize = UIScreen.main.bounds.size
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showBack))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @IBAction func onTapShowBtn(_ sender: UIButton) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "ViewController")
+        
+        let transection = CATransition()
+        transection.duration = 0.5
+        transection.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        transection.type = CATransitionType.push
+        transection.subtype = .fromTop
+        self.view.window?.layer.add(transection, forKey: kCATransition)
+        self.present(vc!, animated: true, completion: nil)
+//        self.navigationController?.view.layer.add(transection, forKey: nil)
+//        navigationController?.pushViewController(vc!, animated: true)
+        
+//        tableView.frame = CGRect(x: 0, y: screenSize.height, width: screenSize.width, height: height)
+//        self.view.addSubview(tableView)
+//
+//        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+////            self.view.backgroundColor = UIColor.gray.withAlphaComponent(0.9)
+//
+//            self.view.alpha = 0.5
+//            self.tableView.frame = CGRect(x: 0, y: self.screenSize.height - self.height, width: self.screenSize.width, height: self.height)
+//        }, completion: nil)
+    }
+    
+    @objc func showBack(){
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+//            self.view.backgroundColor = UIColor.black.withAlphaComponent(1.0)
+            self.view.alpha = 1.0
+            self.tableView.frame = CGRect(x: 0, y: self.screenSize.height, width: self.screenSize.width, height: self.height)
+        }, completion: nil)
+    }
+}
